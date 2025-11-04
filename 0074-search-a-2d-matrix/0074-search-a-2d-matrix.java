d223adf626325b370ed2d6ae64fb7670b649e07a
mahimaapriyadharshinis <mahimaapriyadharshinis@gmail.com>
2025-11-04 21:30:47 +0530

    Time: 0 ms (100%), Space: 44 MB (7.96%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int flag=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+}
