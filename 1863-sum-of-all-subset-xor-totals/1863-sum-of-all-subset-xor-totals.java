617f35ff0eaa7a3d243b961df41006b754f84f05
mahimaapriyadharshinis <mahimaapriyadharshinis@gmail.com>
2026-02-09 07:44:56 +0530

    Time: 0 ms (100%), Space: 42.7 MB (55.03%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int or = 0;
+        for (int x : nums) {
+            or |= x;
+        }
+        return or * (1 << (nums.length - 1));
+    }
+}
